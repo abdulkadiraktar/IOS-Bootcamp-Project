e727b26e08954691a2cfab8304369cee1ba880c7
Abdulkadir Aktar <aktarkadir1@gmail.com>
2024-06-01 14:09:47 +0300

    Initial Commit

---
+++ IOS Bootcamp Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS Bootcamp Project
+//
+//  Created by Abdulkadir Aktar on 1.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
